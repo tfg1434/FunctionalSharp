daeb01bec59a0c2e8e8be92bc194e305b7f9501a
torontofangirl <toronto.tristan@gmail.com>
2021-07-14 13:13:51 -0500

    finish Either

---
+++ Error.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPLibrary {
+    public record Error(string Message) {
+        //peek into IL and find out why point-free wont work
+    }
+}
