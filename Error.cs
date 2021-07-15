323081f4c7a0507afa47f5f0a7b511584f7b9ae2
torontofangirl <toronto.tristan@gmail.com>
2021-07-15 11:15:28 -0500

    base error record

--- Error.cs
+++ Error.cs
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 namespace FPLibrary {
     public record Error(string Message) {
-        //peek into IL and find out why point-free wont work
+        public override string ToString() => $"Error({Message})";
     }
 }
