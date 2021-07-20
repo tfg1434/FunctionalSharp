402f7c5ec96e8ac396a3d98b43f5c686e61190d0
torontofangirl <toronto.tristan@gmail.com>
2021-07-20 11:21:50 -0500

    this is more work than i expected

---
+++ Lst/Lst.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FPLibrary.Lst {
+    using static F;
+
+    //immutable singly linked list
+    public readonly partial struct Lst<T> : IReadOnlyCollection<T>, IEquatable<Lst<T>> {
+        public static Lst<T> Empty => default;
+
+        private Lst(Node head, int count) => (_head, Count) = (head, count);
+
+        public int Count { get; }
+
+        private readonly Node _head;
+        public T Head {
+            get {
+                if (Count == 0) ThrowEmpty();
+                return _head.Value;
+            }
+        }
+
+        public Lst<T> Tail {
+            get {
+                if (Count == 0) ThrowEmpty();
+                return new Lst<T>(_head.Next, Count - 1);
+            }
+        }
+
+        private static void ThrowEmpty() => throw new InvalidOperationException("The list is empty");
+
+        private static Lst<T> create(IEnumerable<T> items) {
+
+        }
+    }
+}
---
+++ Lst/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FPLibrary.Lst {
+    public partial struct Lst<T> {
+        [DebuggerDisplay("Value = {Value}")]
+        internal sealed class Node {
+            internal Node Next { get; }
+            internal readonly T Value;
+
+            internal Node(T value) => Value = value;
+        }
+    }
+}
