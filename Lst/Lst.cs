0c7ab7e37970d9cdf1e882929172e5edc8d3720b
torontofangirl <toronto.tristan@gmail.com>
2021-07-25 15:52:24 -0500

    updated Lst to nullable ref type

--- Lst/Lst.cs
+++ Lst/Lst.cs
@@ -9,6 +9,8 @@ namespace FPLibrary {
     public static partial class F {
         public static Lst<T> List<T>(IEnumerable<T> items) => Lst<T>.Create(items);
 
+        public static Lst<T> List<T>(params T[] items) => Lst<T>.Create(items);
+
         public static Lst<T> ToLst<T>(this IEnumerable<T> src) => List(src);
     }
 
@@ -16,76 +18,70 @@ namespace FPLibrary {
     public readonly partial struct Lst<T> : IEquatable<Lst<T>> {
         public static Lst<T> Empty => default;
 
-        private Lst(Node head, int count) => (_head, Count) = (head, count);
+        private Lst(Node? head, int count) => (_head, Count) = (head, count);
 
         public int Count { get; }
 
-        private readonly Node _head;
+        private readonly Node? _head;
         public T Head {
             get {
                 if (Count == 0) ThrowEmpty();
-                return _head.Value;
+                return _head!.Value;
             }
         }
-
         public Lst<T> Tail {
             get {
                 if (Count == 0) ThrowEmpty();
-                return new Lst<T>(_head.Next, Count - 1);
+                return new Lst<T>(_head!.Next, Count - 1);
             }
         }
 
         public void Deconstruct(out T head, out Lst<T> tail) {
             if (Count == 0) ThrowEmpty();
-            head = _head.Value;
+            head = _head!.Value;
             tail = new Lst<T>(_head.Next, Count - 1);
         }
 
         public Maybe<(T head, Lst<T> tail)> Deconstruct() {
             if (Count != 0) {
-                return Just((_head.Value, 
+                return Just((_head!.Value, 
                     new Lst<T>(_head.Next, Count - 1)));
             }
             return Nothing;
         }
 
         internal static Lst<T> Create(IEnumerable<T> items) {
-            if (items is null) throw new ArgumentNullException(nameof(items));
-            return CreateRange(items);
-        }
-
-        private static void ThrowEmpty() => throw new InvalidOperationException("The list is empty");
-
-        private static Lst<T> CreateRange(IEnumerable<T> items) {
+            //if (items is null) throw new ArgumentNullException(nameof(items));
             if (items is Lst<T> list) return list;
 
             int count = 0;
-            Node head = items
+            Node? head = items
                 .Reverse()
                 .Aggregate(
-                    default(Node),
+                    default(Node), //null
                     (acc, t) => {
                         count++;
                         return new Node(t, acc);
                     }
                 );
 
-            return count == 0 ? Empty : new Lst<T>(head, count);
+            return head is null ? Empty : new Lst<T>(head, count);
         }
 
-        private static (Node newHead, Node newLast) CopyNonEmptyRange(Node head, Node last) {
-            if (head is null) throw new InvalidOperationException("head cannot be null");
-            if (head == last) throw new InvalidOperationException("range was empty");
-        }
+        private static void ThrowEmpty() => throw new InvalidOperationException("The list is empty");
+
+        //private static (Node newHead, Node newLast) CopyNonEmptyRange(Node head, Node? last) {
+        //    if (head == last) throw new InvalidOperationException("range was empty");
+        //}
 
         bool IEquatable<Lst<T>>.Equals(Lst<T> other) => _head == other._head;
 
-        public override bool Equals(object obj) => obj is Lst<T> list && ((IEquatable<Lst<T>>)this).Equals(list);
+        public override bool Equals(object? obj) => obj is Lst<T> list && ((IEquatable<Lst<T>>)this).Equals(list);
 
-        public override int GetHashCode() => _head.GetHashCode();
+        public override int GetHashCode() => _head is null ? 0 : _head.GetHashCode();
 
         public static bool operator ==(Lst<T> self, Lst<T> other) => self.Equals(other);
 
-        public static bool operator !=(Lst<T> self, Lst<T> other) => self != other;
+        public static bool operator !=(Lst<T> self, Lst<T> other) => !(self == other);
     }
 }
--- Lst/LstNode.cs
+++ Lst/LstNode.cs
@@ -3,12 +3,13 @@ using System.Diagnostics;
 
 namespace FPLibrary {
     public readonly partial struct Lst<T> {
-        [DebuggerDisplay("Value = {Value}")]
-        internal sealed class Node {
-            internal Node Next { get; }
+        [DebuggerDisplay("Value = {" + nameof(Value) + "}")]
+        internal sealed record Node {
+            internal Node? Next { get; }
             internal readonly T Value;
 
-            internal Node(T value, Node next) => (Value, Next) = (value, next);
+            internal Node(T value) => Value = value;
+            internal Node(T value, Node? next) => (Value, Next) = (value, next);
         }
     }
 }
