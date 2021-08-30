19fba36a0779e1b79cc64ef4513a7a1ea855448e
torontofangirl <toronto.tristan@gmail.com>
2021-08-29 20:33:25 -0500

    Try delegate

---
+++ Try.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPLibrary {
+    public delegate Exceptional<T> Try<T>();
+
+    public static partial class F {
+        public static Try<T> Try<T>(Func<T> f) => () => f();
+    }
+
+    public static class TryExt {
+        public static Exceptional<T> Run<T>(this Try<T> self) {
+            try { return self(); }
+            catch (Exception ex) { return ex; }
+        }
+
+        public static Try<R> Bind<T, R>(this Try<T> self, Func<T, Try<R>> f)
+            => () 
+                => self.Run()
+                    .Match(ex => ex, t => f(t).Run());
+
+        public static Try<R> Map<T, R>(this Try<T> self, Func<T, R> f)
+            => ()
+                => self.Run()
+                    .Match<Exceptional<R>>(ex => ex, t => f(t));
+
+        public static Try<Func<T2, R>> Map<T1, T2, R>(this Try<T1> self, Func<T1, T2, R> f)
+            => self.Map(f.CurryFirst());
+    }
+}
