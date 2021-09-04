0b9f184243b93e460c02fbe0aef22f978e69dbb1
torontofangirl <toronto.tristan@gmail.com>
2021-09-04 18:11:23 -0500

    started Maybe tests

--- FPLibrary.Tests/Maybe/MaybeTests.cs
+++ FPLibrary.Tests/Maybe/MaybeTests.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Specialized;
 using FsCheck;
 using Xunit;
 using static FPLibrary.F;
 using static FPLibrary.Tests.Utils;
 using Unit = System.ValueTuple;
+using FPLibrary;
+using FsCheck.Xunit;
 
 namespace FPLibrary.Tests.Maybe {
     static class ArbitraryMaybe {
@@ -17,5 +20,32 @@ namespace FPLibrary.Tests.Maybe {
     }
 
     public class MaybeTests {
+        [Fact]
+        public void Match_Int_Just() {
+            GetValue(true)
+                .Match(
+                    Fail,
+                    _ => Succeed());
+        }
+
+        [Fact]
+        public void Match_String_Nothing() {
+            GetValue(false)
+                .Match(
+                    Succeed,
+                    _ => Fail());
+        }
+
+        [Property(Arbitrary = new[] { typeof(ArbitraryMaybe) })]
+        public void LinqQuery_SingleClause_Just(Maybe<int> m) {
+            Maybe<int> expected = m.Map(times2);
+            Maybe<int> actual = from x in m 
+                                select times2(x);
+            
+            Assert.Equal(expected, actual);
+        }
+
+        private Maybe<int> GetValue(bool select)
+            => select ? Just(0) : Nothing;
     }
 }
--- FPLibrary.Tests/Utils.cs
+++ FPLibrary.Tests/Utils.cs
@@ -4,6 +4,7 @@ using Xunit;
 namespace FPLibrary.Tests {
     public static class Utils {
         public static void Fail() => Assert.True(false);
+        public static void Succeed() => Assert.True(true);
 
         public static Func<int, int> plus5 = x => x + 5;
         public static Func<int, int> times2 = x => x * 2;
