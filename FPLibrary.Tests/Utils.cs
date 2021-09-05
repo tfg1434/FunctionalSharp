897961579a8f1ca82de202f710c68438bc030b4d
torontofangirl <toronto.tristan@gmail.com>
2021-09-05 11:28:14 -0500

    Applicate tests

--- FPLibrary.Tests/Utils.cs
+++ FPLibrary.Tests/Utils.cs
@@ -8,6 +8,16 @@ namespace FPLibrary.Tests {
 
         public static readonly Func<int, int> Plus5 = x => x + 5;
         public static readonly Func<int, int> Times2 = x => x * 2;
-        public static readonly Func<int, int, int> Multiply = (i, j) => i * j;
+        public static readonly Func<int, int, int> Add = (i, j) => i + j;
+        public static readonly Func<int, int, int, int> Add3 =
+            (a, b, c) => a + b + c;
+        public static readonly Func<int, int, int, int, int> Add4 =
+            (a, b, c, d) => a + b + c + d;
+        public static readonly Func<int, int, int, int, int, int> Add5 =
+            (a, b, c, d, e) => a + b + c + d + e;
+        public static readonly Func<int, int, int, int, int, int, int> Add6 =
+            (a, b, c, d, e, f) => a + b + c + d + e + f;
+        public static readonly Func<int, int, int, int, int, int, int, int> Add7 =
+            (a, b, c, d, e, f, g) => a + b + c + d + e + f + g;
     }
 }
--- FPTest/Program.cs
+++ FPTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Immutable;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Schema;
 using static FPLibrary.F;
 using FPLibrary;
 using Unit = System.ValueTuple;
@@ -9,11 +10,12 @@ using static System.Diagnostics.Debug;
 
 #nullable enable
 
-Func<int, int> Times2 = x => x * 2;
-Func<int, int> Plus5 = x => x + 5;
+Func<int, int, int> Add = (x, y) => x + y;
 
-Maybe<int> m = Just(5);
+Maybe<int> expected = Just(3);
+Maybe<int> actual = Just(Add)
+    .Apply(Just(1))
+    .Apply(Just(2));
 
-Maybe<int> expected = m.Map(Times2).Map(Plus5);
-Maybe<int> actual = m.Map(x => Times2(Plus5(x)));
+int a = 5;
 
