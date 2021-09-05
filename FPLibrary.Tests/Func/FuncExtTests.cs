f676d887ebf12e952ef46248201aa816870498d2
torontofangirl <toronto.tristan@gmail.com>
2021-09-05 13:26:41 -0500

    all ToFunc overloads

---
+++ FPLibrary.Tests/Func/FuncExtTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xunit;
+using static FPLibrary.F;
+using static FPLibrary.Tests.Utils;
+
+namespace FPLibrary.Tests.Func {
+    public class FuncExtTests {
+        [Fact]
+    }
+}
