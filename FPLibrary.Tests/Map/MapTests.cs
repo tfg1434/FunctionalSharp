2785e1d7368c6c7fce21a3dfc925e734e534ad22
torontofangirl <toronto.tristan@gmail.com>
2021-10-10 10:13:11 -0500

    e q u a l i t y

--- FPLibrary.Tests/Map/MapTests.cs
+++ FPLibrary.Tests/Map/MapTests.cs
@@ -116,7 +116,7 @@ namespace FPLibrary.Tests.Map {
             var map = Map<int, bool>();
             map = map.Add(key, val);
         
-            Assert.Equal(map, map.AddRange(new[] { (key, val) }));
+            Assert.Equal(map, map.AddRange((key, val)));
         }
 
         [Property]
@@ -124,7 +124,7 @@ namespace FPLibrary.Tests.Map {
             var map = Map<int, bool>();
             map = map.Add(key, val);
         
-            Assert.Throws<ArgumentException>(() => map.AddRange(new[] { (key, !val) }));
+            Assert.Throws<ArgumentException>(() => map.AddRange((key, !val)));
         }
         
         [Fact]
@@ -238,6 +238,38 @@ namespace FPLibrary.Tests.Map {
             var map = dict.ToMap();
 
             Assert.Equal(dict.ContainsKey(key), map.Get(key).IsJust);
+        }
+
+        [Fact]
+        public void Equals_Operator_Equals() {
+            var lhs = Map<int, bool>((0, true), (4, false));
+            var rhs = Map<int, bool>((0, true), (4, false));
+            
+            Assert.True(lhs == rhs);
+        }
+
+        [Fact]
+        public void Equals_Method_Equals() {
+            var lhs = Map<int, bool>((0, true), (4, false));
+            var rhs = Map<int, bool>((0, true), (4, false));
+            
+            Assert.True(lhs.Equals(rhs));
+        }
+        
+        [Fact]
+        public void Equals_Operator_NotEqual() {
+            var lhs = Map<int, bool>((0, true), (4, false));
+            var rhs = Map<int, bool>((1, true), (4, false));
+            
+            Assert.False(lhs == rhs);
+        }
+
+        [Fact]
+        public void Equals_Method_NotEqual() {
+            var lhs = Map<int, bool>((0, true), (4, false));
+            var rhs = Map<int, bool>((1, true), (4, false));
+            
+            Assert.False(lhs.Equals(rhs));
         }
     }
 }
---
+++ FPLibrary/MapIEquatable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FPLibrary {
+    public sealed partial class Map<K, V> : IEquatable<Map<K, V>> where K : notnull {
+        public static bool operator ==(Map<K, V> self, Map<K, V> other)
+            => self.Equals(other);
+
+        public static bool operator !=(Map<K, V> self, Map<K, V> other)
+            => !(self == other);
+
+        public override bool Equals(object? obj)
+            => obj is Map<K, V> map && Equals(map);
+
+        public bool Equals(Map<K, V>? other) {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (Count != other.Count) return false;
+            //if (hashCode != 0 && other.hashCode != 0) return false;
+
+            using var iterThis = GetEnumerator();
+            using var iterOther = other.GetEnumerator();
+
+            for (int i = 0; i < Count; i++) {
+                iterThis.MoveNext();
+                iterOther.MoveNext();
+                
+                if (keyComparer.Compare(iterThis.Current.Key, iterOther.Current.Key) != 0) return false;
+                if (!valComparer.Equals(iterThis.Current.Val, iterOther.Current.Val)) return false;
+            }
+
+            return true;
+        }
+    }
+}
