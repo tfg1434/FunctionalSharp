7cdd6e98667a69f3c768ee5daf8c11ab7d1967cf
torontofangirl <toronto.tristan@gmail.com>
2021-10-16 14:35:50 -0500

    maybe it works

--- FPLibrary/Range.cs
+++ FPLibrary/Range.cs
@@ -1,49 +1,88 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using FPLibrary;
+using static FPLibrary.F;
 
 namespace FPLibrary {
-    public class Range<T> : IEnumerable<T> {
-        public readonly T From;
-        //null = infinite
-        public readonly T? To;
-        public readonly T Step;
-        public readonly bool IsAscending;
+    public static partial class F {
+        public static IEnumerable<int> Range(string range)
+            => FPLibrary.Range.Of(range).IfNothing(
+                () => throw new ArgumentException("Invalid range.", nameof(range)));
+    }
+    
+    public class Range : IEnumerable<int> {
+        private readonly int _from;
+        private readonly int? _to;
+        private readonly int _step;
+        private readonly bool _isAscending;
 
-        private readonly IComparer<T> _comparer = Comparer<T>.Default;
-        private readonly Func<T, T, T> _plus;
+        private bool isInfinite => _to is null;
 
-        protected Range(T from, T? to, T step, IComparer<T> comparer, Func<T, T, T> plus) {
-            From = from;
-            To = to;
-            Step = step;
-            // ReSharper disable once ArrangeDefaultValueWhenTypeNotEvident
-            IsAscending = _comparer.Compare(step, default(T)) >= 0;
-            _comparer = comparer;
-            _plus = plus;
+        private Range(int from, int? to, int step = 1) {
+            _from = from;
+            _to = to;
+            _step = step;
+            _isAscending = step > 0;
         }
-        
-        public bool Contains(T value) {
-            T? from = _comparer.Compare(From, To) > 0 ? To : From;
-            T? to = _comparer.Compare(From, To) > 0 ? From : To;
 
-            return _comparer.Compare(value, from) >= 0 && _comparer.Compare(value, to) <= 0;
-        }
-        
-        public IEnumerable<T> AsEnumerable() {
-            if (IsAscending) {
-                for (T x = From; To is null || _comparer.Compare(x, To) <= 0; x = _plus(x, Step)) {
-                    yield return x;
-                }
-            } else {
-                for (T x = From; To is null || _comparer.Compare(x, To) >= 0; x = _plus(x, Step)) {
-                    yield return x;
-                }
+        public static Maybe<Range> Of(string range) {
+            const string pattern_from_second_to = @"^(?<from>[0-9]+)(?:,(?<second>[0-9]+))\.\.(?<to>[0-9]+)$";
+            const string pattern_from_second    = @"^(?<from>[0-9]+)(?:,(?<second>[0-9]+))\.\.$";
+            const string pattern_from_to        = @"^(?<from>[0-9]+)\.\.(?<to>[0-9]+)$";
+            const string pattern_from           = @"^(?<from>[0-9]+)\.\.$";
+
+            var match = Regex.Match(range, pattern_from_second_to);
+
+            if (match.Success) {
+                var from = int.Parse(match.Groups["from"].Value);
+                var second = int.Parse(match.Groups["second"].Value);
+                var to = int.Parse(match.Groups["to"].Value);
+                
+                return new Range(from, to, second - from);
             }
+
+            match = Regex.Match(range, pattern_from_second);
+
+            if (match.Success) {
+                var from = int.Parse(match.Groups["from"].Value);
+                var second = int.Parse(match.Groups["second"].Value);
+
+                return new Range(from, null, second - from);
+            }
+
+            match = Regex.Match(range, pattern_from_to);
+
+            if (match.Success) {
+                var from = int.Parse(match.Groups["from"].Value);
+                var to = int.Parse(match.Groups["to"].Value);
+
+                return new Range(from, to);
+            }
+
+            match = Regex.Match(range, pattern_from);
+
+            if (match.Success) {
+                var from = int.Parse(match.Groups["from"].Value);
+
+                return new Range(from, null);
+            }
+
+            return Nothing;
         }
 
-        public IEnumerator<T> GetEnumerator() => AsEnumerable().GetEnumerator();
-
+        public IEnumerable<int> AsEnumerable() {
+            if (_isAscending)
+                for (int x = _from; isInfinite || x <= _to; x += _step)
+                    yield return x;
+            else
+                for (int x = _from; isInfinite || x >= _to; x += _step)
+                    yield return x;
+        }
+        
+        public IEnumerator<int> GetEnumerator() => AsEnumerable().GetEnumerator();
+        
         IEnumerator IEnumerable.GetEnumerator() => AsEnumerable().GetEnumerator();
     }
 }
---
+++ FPLibrary/RangeInt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FPLibrary {
+    // public class RangeInt : Range<int> {
+    //     protected RangeInt(int from, int? to, int step, Func<int, int, int> compare, Func<int, int, int> plus) 
+    //         : base(from, to, step, compare, plus) { }
+    //
+    //     internal static Range(int from, int? to, int step) {
+    //         RangeInt(from, to, step, Comparer<int>.Default, (x, y) => x + y);
+    //     }
+    // }
+}
