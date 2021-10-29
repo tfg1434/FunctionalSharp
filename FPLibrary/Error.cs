655fe7b2cc061b4c7a914a5017ef11b3a911ae9a
torontofangirl <toronto.tristan@gmail.com>
2021-10-28 20:48:40 -0500

    idk lol

--- FPLibrary/Error.cs
+++ FPLibrary/Error.cs
@@ -1,5 +1,7 @@
-﻿namespace FPLibrary {
-    public record Error(string Message) {
+﻿using System;
+
+namespace FPLibrary {
+    public record Error(int Code, string Message, Exception Exception) {
         public override string ToString() => $"Error({Message})";
     }
 }
