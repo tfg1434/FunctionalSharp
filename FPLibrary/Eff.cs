f10ace9c8c4b57a7929e67f4bbffc0e8de68647e
torontofangirl <toronto.tristan@gmail.com>
2021-11-12 06:07:05 -0600

    map, bimap, match, bind

---
+++ FPLibrary/Eff.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Unit = System.ValueTuple;
+using static FPLibrary.F;
+
+namespace FPLibrary; 
+
+public static partial class F {
+    public static Eff<T> Eff<T>(Func<Result<T>> f) 
+        => new(Thunk<T>.Of(f));
+
+    public static Eff<T> EffLazy<T>(Func<T> f)
+        => new(Thunk<T>.Of(() => new(f())));
+
+    public static Eff<T> EffSucc<T>(T value)
+        => new(Thunk<T>.OfSucc(value));
+
+    public static Eff<T> EffFail<T>(Error error)
+        => new(Thunk<T>.OfFail(error));
+}
+
+public readonly struct Eff<T> {
+    private readonly Thunk<T> _thunk;
+
+    internal Eff(Thunk<T> thunk) 
+        => _thunk = thunk;
+
+    public Result<T> Run() => _thunk.Value();
+    
+    public Result<T> ReRun() => _thunk.ReValue();
+
+    public Eff<T> Clone() => new(_thunk.Clone());
+
+    public Eff<R> Map<R>(Func<T, R> f)
+        => new(_thunk.Map(f));
+
+    public Eff<R> BiMap<R>(Func<T, R> succ, Func<Error, Error> fail)
+        => new(_thunk.BiMap(succ, fail));
+
+    public Eff<R> Match<R>(Func<T, R> succ, Func<Error, R> fail) {
+        var @this = this;
+        
+        return EffLazy(() => {
+            Result<T> res = @this.ReRun();
+
+            return res.Match(fail, succ);
+        });
+    }
+
+    public Eff<R> Bind<R>(Func<T, Eff<R>> f) {
+        var @this = this;
+        
+        return new(Thunk<R>.Of(() => {
+            Result<T> res = @this.ReRun();
+
+            return res.Match(Result<R>.Of, t => f(t).Run());
+        }));
+    }
+
+    public Eff<T> IfFail(Func<Error, T> f) {
+        var @this = this;
+        
+        return Eff(() => {
+            var res = @this.ReRun();
+            
+            return res.IsSucc ? res : Result<T>.Of(f(res.Error!));
+        });
+    }
+
+    public Eff<T> IfFail(Func<T> f) {
+        var @this = this;
+        
+        return Eff(() => {
+            var res = @this.ReRun();
+            
+            return res.IsSucc ? res : Result<T>.Of(f());
+        });
+    }
+    
+    public Eff<T> IfFail(T value) {
+        var @this = this;
+        
+        return Eff(() => {
+            var res = @this.ReRun();
+            
+            return res.IsSucc ? res : Result<T>.Of(value);
+        });
+    }
+
+    public Eff<R> Select<R>(Func<T, R> f) => Map(f);
+    
+    public Eff<PR> SelectMany<R, PR>(Func<T, Eff<R>> bind, Func<T, R, PR> proj)
+        => Bind(t => bind(t).Map(r => proj(t, r)));
+}
--- FPLibrary/IO.cs
+++ FPLibrary/IO.cs
@@ -5,99 +5,53 @@ using static FPLibrary.F;
 namespace FPLibrary; 
 
 public static partial class F {
-    public static IO<T> IO<T>(Func<Result<T>> f) 
-        => new(Thunk<T>.Of(f));
+    public static IO<E, T> IO<E, T>(Func<E, Result<T>> f) where E : struct
+        => new(Thunk<E, T>.Of(f));
 
-    public static IO<T> IOLazy<T>(Func<T> f)
-        => new(Thunk<T>.Of(() => new(f())));
+    public static IO<E, T> IOLazy<E, T>(Func<E, T> f) where E : struct
+        => new(Thunk<E, T>.Of(env => new(f(env))));
 
-    public static IO<T> IOSucc<T>(T value)
-        => new(Thunk<T>.OfSucc(value));
+    public static IO<E, T> IOSucc<E, T>(T value) where E : struct
+        => new(Thunk<E, T>.OfSucc(value));
 
-    public static IO<T> IOFail<T>(Error error)
-        => new(Thunk<T>.OfFail(error));
+    public static IO<E, T> IOFail<E, T>(Error error) where E : struct
+        => new(Thunk<E, T>.OfFail(error));
 }
 
-public readonly struct IO<T> {
-    private readonly Thunk<T> _thunk;
-
-    internal IO(Thunk<T> thunk) 
-        => _thunk = thunk;
-
-    public Result<T> Run() => _thunk.Value();
+public readonly struct IO<E, T> where E : struct {
+    private readonly Thunk<E, T> _thunk;
     
-    public Result<T> ReRun() => _thunk.ReValue();
+    internal IO(Thunk<E, T> thunk) => _thunk = thunk;
 
-    public IO<T> Clone() => new(_thunk.Clone());
+    public Result<T> Run(E env) => _thunk.Value(env);
 
-    public IO<R> Map<R>(Func<T, R> f)
+    public Result<T> ReRun(E env) => _thunk.ReValue(env);
+
+    public IO<E, T> Clone() => new(_thunk.Clone());
+
+    public IO<E, R> Map<R>(Func<T, R> f)
         => new(_thunk.Map(f));
 
-    public IO<R> BiMap<R>(Func<T, R> succ, Func<Error, Error> fail)
+    public IO<E, R> BiMap<R>(Func<T, R> succ, Func<Error, Error> fail)
         => new(_thunk.BiMap(succ, fail));
 
-    public IO<R> Match<R>(Func<T, R> succ, Func<Error, R> fail) {
+    public IO<E, R> Match<R>(Func<T, R> succ, Func<Error, R> fail) {
         var @this = this;
-        
-        return IOLazy(() => {
-            Result<T> res = @this.ReRun();
 
-            return res.IsSucc 
-                ? succ(res.Value!) 
-                : fail(res.Error!);
+        return IOLazy<E, R>(env => {
+            Result<T> res = @this.Run(env);
+            
+            return res.Match(fail, succ);
         });
     }
 
-    public IO<R> Bind<R>(Func<T, IO<R>> f) {
+    public IO<E, R> Bind<R>(Func<T, IO<E, R>> f) {
         var @this = this;
-        
-        return new(Thunk<R>.Of(() => {
-            Result<T> res = @this.ReRun();
 
-            if (res.IsFail)
-                return Result<R>.Of(res.Error!);
+        return new(Thunk<E, R>.Of(env => {
+            Result<T> res = @this.Run(env);
 
-            return f(res.Value!).Run();
+            return res.Match(Result<R>.Of, t => f(t).Run(env));
         }));
     }
-
-    public IO<T> IfFail(Func<Error, T> f) {
-        var @this = this;
-        
-        return IO(() => {
-            var res = @this.ReRun();
-            
-            return res.IsSucc ? res : Result<T>.Of(f(res.Error!));
-        });
-    }
-
-    public IO<T> IfFail(Func<T> f) {
-        var @this = this;
-        
-        return IO(() => {
-            var res = @this.ReRun();
-            
-            return res.IsSucc ? res : Result<T>.Of(f());
-        });
-    }
-    
-    public IO<T> IfFail(T value) {
-        var @this = this;
-        
-        return IO(() => {
-            var res = @this.ReRun();
-            
-            return res.IsSucc ? res : Result<T>.Of(value);
-        });
-    }
-
-    public IO<R> Select<R>(Func<T, R> f) => Map(f);
-    
-    public IO<PR> SelectMany<R, PR>(Func<T, IO<R>> bind, Func<T, R, PR> proj)
-        => Bind(t => bind(t).Map(r => proj(t, r)));
-}
-
-public static class IOExt
-{
-    
 }
