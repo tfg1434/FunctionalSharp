aea09683ac2afa5281c05c4b3d806b0b204c245b
torontofangirl <toronto.tristan@gmail.com>
2021-11-13 16:17:48 -0600

    architecture

---
+++ FPLibrary.Wrappers/IO/File.cs
@@ -0,0 +1,11 @@
+﻿using Unit = System.ValueTuple;
+using FPLibrary;
+
+namespace FPLibrary.Wrappers.IO;
+
+public static class File<E> where E : struct, IHasFile<E> {
+    public static IO<E, Unit> Copy(string from, string to, bool overwrite = false)
+        => default(E).FileIO.Map(e => e.Copy(from, to, overwrite));
+    
+    
+}
---
+++ FPLibrary.Wrappers/IO/IFileIO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FPLibrary;
+using Unit = System.ValueTuple;
+
+namespace FPLibrary.Wrappers.IO;
+
+public interface IHasFile<E> where E : struct {
+    IO<E, IFileIO> FileIO { get; }
+}
+
+public interface IFileIO {
+    public Unit Copy(string from, string to, bool overwrite = false);
+}
