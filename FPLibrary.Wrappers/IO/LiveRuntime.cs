665988e14a4b52c0430817515ff8d92b37b0e9b5
torontofangirl <toronto.tristan@gmail.com>
2021-11-14 06:55:36 -0600

    live runtime bitches

---
+++ FPLibrary.Wrappers/IO/LiveRuntime.cs
@@ -0,0 +1,19 @@
+﻿using static FPLibrary.F;
+
+namespace FPLibrary.Wrappers.IO; 
+
+public readonly struct LiveRuntime : IHasFile<LiveRuntime> {
+    private LiveRuntime(LiveRuntimeEnv env)
+        => _env = env;
+
+    public LiveRuntime() : this(new()) { }
+
+    public LiveRuntimeEnv Env => _env ?? throw new InvalidOperationException("null, did you use the ctor?");
+
+    public IO<LiveRuntime, IFileIO> FileIO 
+        => IOSucc<LiveRuntime, IFileIO>(LiveFileIO.Default);
+    
+    private readonly LiveRuntimeEnv? _env;
+}
+
+public class LiveRuntimeEnv { }
