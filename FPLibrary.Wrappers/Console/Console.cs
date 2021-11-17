a08b10d2a42acc1d41e0392ae5dcfe6ebc343bd0
torontofangirl <toronto.tristan@gmail.com>
2021-11-17 06:38:25 -0600

    todo: implicit conversions

---
+++ FPLibrary.Wrappers/Console/Console.cs
@@ -0,0 +1,21 @@
+﻿using Unit = System.ValueTuple;
+using static FPLibrary.F;
+
+namespace FPLibrary.Wrappers.Console;
+
+public static class Console<E> where E : struct, IHasConsole<E> {
+    public static IO<E, Unit> Clear()
+        => default(E).ConsoleIO.Map(env => env.Clear());
+    
+    public static IO<E, int> Read()
+        => default(E).ConsoleIO
+            .Map(env => env.Read()
+                .Match(
+                    () => EffFail<int>(new IOError("End of stream")),
+                    EffSucc<int>));
+
+    public static IO<E, ConsoleKeyInfo> ReadKey()
+        => default(E).ConsoleIO.Map(env => env.ReadKey());
+    
+    
+}
