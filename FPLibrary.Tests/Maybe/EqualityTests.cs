ffc4c6c842f0b08282351fcd782012e99f245a4f
torontofangirl <toronto.tristan@gmail.com>
2021-11-17 05:36:59 -0600

    maybe tests

--- FPLibrary.Tests/Maybe/EqualityTests.cs
+++ FPLibrary.Tests/Maybe/EqualityTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using FsCheck.Xunit;
+using Xunit;
 using static FPLibrary.F;
 
 namespace FPLibrary.Tests.Maybe; 
@@ -12,6 +13,19 @@ public class EqualityTests {
         Assert.Equal(a, b);
         Assert.Equal(a.GetHashCode(), b.GetHashCode());
     }
-    
-    // [Fact]
+
+    [Property(Arbitrary = new[] { typeof(ArbitraryMaybe) })]
+    public void HashCode_NoThrow_Holds(Maybe<int> m) {
+        m.GetHashCode();
+    }
+
+    [Property(Arbitrary = new[] { typeof(ArbitraryMaybe) })]
+    public void Equals_Is_Equal(int n) {
+        var a = Just(n);
+        var b = Just(n);
+        
+        Assert.True(a == b);
+        Assert.False(a != b);
+        Assert.Equal(a, b);
+    }
 }
