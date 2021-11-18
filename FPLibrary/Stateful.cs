eba589356d5de39abf3b7896824ceae237f7e789
torontofangirl <toronto.tristan@gmail.com>
2021-11-18 06:39:09 -0600

    Select & SelectMany

--- FPLibrary/Stateful.cs
+++ FPLibrary/Stateful.cs
@@ -10,19 +10,26 @@ public static class StatefulExt {
     public static T Run<S, T>(this Stateful<S, T> f, S state) 
         => f(state).Value;
     
-    public static Stateful<S, R> Bind<S, T, R>(this Stateful<S, T> f, Func<T, Stateful<S, R>> proj)
+    public static Stateful<S, R> Bind<S, T, R>(this Stateful<S, T> self, Func<T, Stateful<S, R>> f)
         => state => {
-            (T t, S newState) = f(state);
+            (T t, S newState) = self(state);
             
-            return proj(t)(newState);
+            return f(t)(newState);
         };
 
-    public static Stateful<S, R> Map<S, T, R>(this Stateful<S, T> f, Func<T, R> proj)
+    public static Stateful<S, R> Map<S, T, R>(this Stateful<S, T> self, Func<T, R> f)
         => state => {
-            (T t, S newState) = f(state);
+            (T t, S newState) = self(state);
             
-            return (proj(t), newState);
+            return (f(t), newState);
         };
+
+    public static Stateful<S, R> Select<S, T, R>(this Stateful<S, T> self, Func<T, R> f)
+        => Map(self, f);
+
+    public static Stateful<S, PR> SelectMany<S, T, R, PR>(this Stateful<S, T> self, Func<T, Stateful<S, R>> bind,
+        Func<T, R, PR> proj)
+        => self.Bind(t => bind(t).Map(r => proj(t, r)));
 
     // public static IEnumerable<T> AsEnumerable<T, S>(this Stateful<T, S> self)
 
