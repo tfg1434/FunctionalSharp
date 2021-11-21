203c0447451c27b0c9cd03ebc086752e81a91b74
torontofangirl <toronto.tristan@gmail.com>
2021-11-21 13:10:35 -0600

    nullable docs

--- FunctionalSharp/NullableExt.cs
+++ FunctionalSharp/NullableExt.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using FunctionalSharp;
-using static FunctionalSharp.F;
+﻿using System.Diagnostics.Contracts;
 
 namespace FunctionalSharp; 
 
 public static class NullableExt {
+    /// <summary>
+    /// Turn a nullable into a <see cref="Maybe{T}"/>
+    /// </summary>
+    [Pure]
     public static Maybe<T> ToMaybe<T>(this T? self) where T : struct
         => self.HasValue ? Just(self.Value) : Nothing;
 }
