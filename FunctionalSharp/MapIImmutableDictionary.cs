f50c9e5b3e014545d63b15d2524fabfd282e56e5
torontofangirl <toronto.tristan@gmail.com>
2021-11-22 12:53:54 -0600

    IImmutableDictionary

--- FunctionalSharp/MapIImmutableDictionary.cs
+++ FunctionalSharp/MapIImmutableDictionary.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 
 namespace FunctionalSharp; 
 
 public sealed partial class Map<K, V> : IImmutableDictionary<K, V> where K : notnull {
-    IImmutableDictionary<K, V> IImmutableDictionary<K, V>.Append(K key, V value) 
+    #region IImmutableDictionary
+
+    IImmutableDictionary<K, V> IImmutableDictionary<K, V>.Add(K key, V value) 
         => Append((key, value));
 
-    IImmutableDictionary<K, V> IImmutableDictionary<K, V>.Append(IEnumerable<KeyValuePair<K, V>> items)
+    IImmutableDictionary<K, V> IImmutableDictionary<K, V>.AddRange(IEnumerable<KeyValuePair<K, V>> items)
         => Append(items.Map(x => (x.Key, x.Value)));
 
     IImmutableDictionary<K, V> IImmutableDictionary<K, V>.Clear() 
@@ -25,4 +30,62 @@ public sealed partial class Map<K, V> : IImmutableDictionary<K, V> where K : not
 
     IImmutableDictionary<K, V> IImmutableDictionary<K, V>.SetItems(IEnumerable<KeyValuePair<K, V>> items)
         => SetItems(items);
+    
+    #endregion
+
+    #region IReadonlyDictionary
+
+    /// <summary>
+    /// Unsafely get a value from the map
+    /// </summary>
+    /// <param name="key">Key to get from</param>
+    /// <exception cref="KeyNotFoundException">If key does not exist in the map</exception>
+    [Pure]
+    public V this[K key] => Get(key).IfNothing(
+        () => throw new KeyNotFoundException("Key does not exist in map."));
+
+    /// <summary>
+    /// IEnumerable of the keys in the map
+    /// </summary>
+    [Pure]
+    public IEnumerable<K> Keys => _root.Keys;
+        
+    /// <summary>
+    /// IEnumerable of the values in the map
+    /// </summary>
+    [Pure]
+    public IEnumerable<V> Values => _root.Values;
+
+    #endregion
+
+    #region ICollection
+
+    void ICollection.CopyTo(Array array, int index)
+        => _root.CopyTo(array, index, Count);
+    
+    bool ICollection<KeyValuePair<K, V>>.IsReadOnly => true;
+    
+    void ICollection<KeyValuePair<K, V>>.Add(KeyValuePair<K, V> item) => throw new NotSupportedException();
+
+    void ICollection<KeyValuePair<K, V>>.Clear() => throw new NotSupportedException();
+
+    bool ICollection<KeyValuePair<K, V>>.Remove(KeyValuePair<K, V> item)
+        => throw new NotSupportedException();
+
+    void ICollection<KeyValuePair<K, V>>.CopyTo(KeyValuePair<K, V>[] array, int index) {
+        if (array is null) throw new ArgumentNullException(nameof(array));
+        if (index < 0) throw new ArgumentOutOfRangeException(nameof(index));
+        if (array.Length < index + Count) throw new ArgumentOutOfRangeException(nameof(index));
+
+        foreach ((K k, V v) in this)
+            array[index++] = new(k, v);
+    }
+    
+    bool ICollection.IsSynchronized => true;
+
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    object ICollection.SyncRoot => this;
+
+    #endregion
+    
 }
