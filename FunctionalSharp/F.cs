1ddb31aae7c5f9c47c7c150398e44bbea33b5c0c
torontofangirl <toronto.tristan@gmail.com>
2021-11-23 06:22:36 -0600

    F.cs docs

--- FunctionalSharp/F.cs
+++ FunctionalSharp/F.cs
@@ -1,66 +1,135 @@
-﻿using System;
+﻿using System.Diagnostics.Contracts;
 
-namespace FunctionalSharp {
-    public static partial class F {
-        public static Unit Unit() => default;
+namespace FunctionalSharp; 
 
-        public static R Using<TDisp, R>(TDisp disposable, Func<TDisp, R> f)
-            where TDisp : IDisposable {
-            using (disposable) return f(disposable);
-        }
+public static partial class F {
+    /// <summary>
+    /// Unit ctor
+    /// </summary>
+    /// <returns>Unit</returns>
+    [Pure]
+    public static Unit Unit() => default;
 
-        public static Unit Using<TDisp>(TDisp disposable, Action<TDisp> act)
-            where TDisp : IDisposable => Using(disposable, act.ToFunc());
-
-        //this is in here instead of FuncExt so you can do F.CurryFirst as non-extension method.
-        public static Func<T1, Func<T2, R>> Curry<T1, T2, R>(this Func<T1, T2, R> f)
-            => t1 => t2 => f(t1, t2);
-
-        public static Func<T1, Func<T2, Func<T3, R>>> Curry<T1, T2, T3, R>(this Func<T1, T2, T3, R> f)
-            => t1 => t2 => t3 => f(t1, t2, t3);
-
-        public static Func<T1, Func<T2, Func<T3, Func<T4, R>>>> Curry<T1, T2, T3, T4, R>(this Func<T1, T2, T3, T4, R> f)
-            => t1 => t2 => t3 => t4 => f(t1, t2, t3, t4);
-
-        public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, R>>>>> Curry<T1, T2, T3, T4, T5, R>(
-            this Func<T1, T2, T3, T4, T5, R> f)
-            => t1 => t2 => t3 => t4 => t5 => f(t1, t2, t3, t4, t5);
-
-        public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, R>>>>>> Curry<T1, T2, T3, T4, T5, T6, R>(
-            this Func<T1, T2, T3, T4, T5, T6, R> f)
-            => t1 => t2 => t3 => t4 => t5 => t6 => f(t1, t2, t3, t4, t5, t6);
-
-        public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, R>>>>>>> Curry<T1, T2, T3, T4, T5,
-            T6, T7, R>(this Func<T1, T2, T3, T4, T5, T6, T7, R> f)
-            => t1 => t2 => t3 => t4 => t5 => t6 => t7 => f(t1, t2, t3, t4, t5, t6, t7);
-
-        public static Func<T1, Func<T2, R>> CurryFirst<T1, T2, R>(this Func<T1, T2, R> self)
-            => t1 => t2 => self(t1, t2);
-
-        public static Func<T1, Func<T2, T3, R>> CurryFirst<T1, T2, T3, R>(this Func<T1, T2, T3, R> self)
-            => t1 => (t2, t3) => self(t1, t2, t3);
-
-        public static Func<T1, Func<T2, T3, T4, R>> CurryFirst<T1, T2, T3, T4, R>(this Func<T1, T2, T3, T4, R> self)
-            => t1 => (t2, t3, t4) => self(t1, t2, t3, t4);
-
-        public static Func<T1, Func<T2, T3, T4, T5, R>> CurryFirst<T1, T2, T3, T4, T5, R>(
-            this Func<T1, T2, T3, T4, T5, R> self)
-            => t1 => (t2, t3, t4, t5) => self(t1, t2, t3, t4, t5);
-
-        public static Func<T1, Func<T2, T3, T4, T5, T6, R>> CurryFirst<T1, T2, T3, T4, T5, T6, R>(
-            this Func<T1, T2, T3, T4, T5, T6, R> self)
-            => t1 => (t2, t3, t4, t5, t6) => self(t1, t2, t3, t4, t5, t6);
-
-        public static Func<T1, Func<T2, T3, T4, T5, T6, T7, R>> CurryFirst<T1, T2, T3, T4, T5, T6, T7, R>(
-            this Func<T1, T2, T3, T4, T5, T6, T7, R> self)
-            => t1 => (t2, t3, t4, t5, t6, t7) => self(t1, t2, t3, t4, t5, t6, t7);
-
-        public static Func<T1, Func<T2, T3, T4, T5, T6, T7, T8, R>> CurryFirst<T1, T2, T3, T4, T5, T6, T7, T8, R>(
-            this Func<T1, T2, T3, T4, T5, T6, T7, T8, R> self)
-            => t1 => (t2, t3, t4, t5, t6, t7, t8) => self(t1, t2, t3, t4, t5, t6, t7, t8);
-
-        public static Func<T1, Func<T2, T3, T4, T5, T6, T7, T8, T9, R>> CurryFirst<T1, T2, T3, T4, T5, T6, T7, T8, T9,
-            R>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, R> self)
-            => t1 => (t2, t3, t4, t5, t6, t7, t8, t9) => self(t1, t2, t3, t4, t5, t6, t7, t8, t9);
+    /// <summary>
+    /// Using HOF
+    /// </summary>
+    [Pure]
+    public static R Using<T_DISP, R>(T_DISP disposable, Func<T_DISP, R> f) where T_DISP : IDisposable {
+        using (disposable) return f(disposable);
     }
+
+    /// <summary>
+    /// Using HOF
+    /// </summary>
+    [Pure]
+    public static Unit Using<TDisp>(TDisp disposable, Action<TDisp> act) where TDisp : IDisposable 
+        => Using(disposable, act.ToFunc());
+
+    //this is in here instead of FuncExt so you can do F.CurryFirst as non-extension method
+    
+    /// <summary>
+    /// Curry all arguments of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, R>> Curry<T1, T2, R>(this Func<T1, T2, R> f)
+        => t1 => t2 => f(t1, t2);
+
+    /// <summary>
+    /// Curry all arguments of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, Func<T3, R>>> Curry<T1, T2, T3, R>(this Func<T1, T2, T3, R> f)
+        => t1 => t2 => t3 => f(t1, t2, t3);
+
+    /// <summary>
+    /// Curry all arguments of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, Func<T3, Func<T4, R>>>> Curry<T1, T2, T3, T4, R>(this Func<T1, T2, T3, T4, R> f)
+        => t1 => t2 => t3 => t4 => f(t1, t2, t3, t4);
+
+    /// <summary>
+    /// Curry all arguments of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, R>>>>> Curry<T1, T2, T3, T4, T5, R>(
+        this Func<T1, T2, T3, T4, T5, R> f)
+        => t1 => t2 => t3 => t4 => t5 => f(t1, t2, t3, t4, t5);
+
+    /// <summary>
+    /// Curry all arguments of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, R>>>>>> Curry<T1, T2, T3, T4, T5, T6, R>(
+        this Func<T1, T2, T3, T4, T5, T6, R> f)
+        => t1 => t2 => t3 => t4 => t5 => t6 => f(t1, t2, t3, t4, t5, t6);
+
+    /// <summary>
+    /// Curry all arguments of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, R>>>>>>> Curry<T1, T2, T3, T4, T5,
+        T6, T7, R>(this Func<T1, T2, T3, T4, T5, T6, T7, R> f)
+        => t1 => t2 => t3 => t4 => t5 => t6 => t7 => f(t1, t2, t3, t4, t5, t6, t7);
+
+    /// <summary>
+    /// Curry the first argument of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, R>> CurryFirst<T1, T2, R>(this Func<T1, T2, R> self)
+        => t1 => t2 => self(t1, t2);
+    
+    /// <summary>
+    /// Curry the first argument of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, T3, R>> CurryFirst<T1, T2, T3, R>(this Func<T1, T2, T3, R> self)
+        => t1 => (t2, t3) => self(t1, t2, t3);
+
+    /// <summary>
+    /// Curry the first argument of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, T3, T4, R>> CurryFirst<T1, T2, T3, T4, R>(this Func<T1, T2, T3, T4, R> self)
+        => t1 => (t2, t3, t4) => self(t1, t2, t3, t4);
+
+    /// <summary>
+    /// Curry the first argument of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, T3, T4, T5, R>> CurryFirst<T1, T2, T3, T4, T5, R>(
+        this Func<T1, T2, T3, T4, T5, R> self)
+        => t1 => (t2, t3, t4, t5) => self(t1, t2, t3, t4, t5);
+
+    /// <summary>
+    /// Curry the first argument of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, T3, T4, T5, T6, R>> CurryFirst<T1, T2, T3, T4, T5, T6, R>(
+        this Func<T1, T2, T3, T4, T5, T6, R> self)
+        => t1 => (t2, t3, t4, t5, t6) => self(t1, t2, t3, t4, t5, t6);
+
+    /// <summary>
+    /// Curry the first argument of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, T3, T4, T5, T6, T7, R>> CurryFirst<T1, T2, T3, T4, T5, T6, T7, R>(
+        this Func<T1, T2, T3, T4, T5, T6, T7, R> self)
+        => t1 => (t2, t3, t4, t5, t6, t7) => self(t1, t2, t3, t4, t5, t6, t7);
+
+    /// <summary>
+    /// Curry the first argument of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, T3, T4, T5, T6, T7, T8, R>> CurryFirst<T1, T2, T3, T4, T5, T6, T7, T8, R>(
+        this Func<T1, T2, T3, T4, T5, T6, T7, T8, R> self)
+        => t1 => (t2, t3, t4, t5, t6, t7, t8) => self(t1, t2, t3, t4, t5, t6, t7, t8);
+
+    /// <summary>
+    /// Curry the first argument of a function
+    /// </summary>
+    [Pure]
+    public static Func<T1, Func<T2, T3, T4, T5, T6, T7, T8, T9, R>> CurryFirst<T1, T2, T3, T4, T5, T6, T7, T8, T9,
+        R>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, R> self)
+        => t1 => (t2, t3, t4, t5, t6, t7, t8, t9) => self(t1, t2, t3, t4, t5, t6, t7, t8, t9);
 }
